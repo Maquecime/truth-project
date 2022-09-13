756f5fe1c99dd2129f5770ef3dc6ae11014e26c2
Maquecime <mdatin.contact@gmail.com>
2022-09-13 14:14:41 +0200

    feat: front pattern

--- src/components/ActionsView.vue
+++ src/components/ActionsView.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="greetings">
-    <button type="button" @click="generate" class="btn btn-primary">
-      Generate
-    </button>
+    <button class="button-54" @click="generate"> Generate </button>
   </div>
 </template>
 
@@ -36,6 +34,43 @@ h3 {
   .greetings h1,
   .greetings h3 {
     text-align: left;
+  }
+}
+
+/* CSS */
+.button-54 {
+  font-family: "Open Sans", sans-serif;
+  font-size: 16px;
+  letter-spacing: 2px;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: #000;
+  cursor: pointer;
+  border: 3px solid;
+  padding: 0.25em 0.5em;
+  box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px;
+  position: relative;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-54:active {
+  box-shadow: 0px 0px 0px 0px;
+  top: 5px;
+  left: 5px;
+}
+
+@media (min-width: 768px) {
+  .button-54 {
+    padding: 0.25em 0.75em;
+  }
+}
+
+@media (min-width: 768px) {
+  .button-53 {
+    padding: .75rem 3rem;
+    font-size: 1.25rem;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import router from "./router";
 import "./assets/main.css";
 import store from "./store";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
 
 const app = createApp(App).use(store);
 
 app.use(router);
+app.use(Antd);
 
 app.mount("#app");
